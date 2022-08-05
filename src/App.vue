1fd9849733e4285e914633f9ab0544b08da865b1
Andrew1451 <44391620+Andrew1451@users.noreply.github.com>
2022-08-04 23:24:47 -0400

    create awesome site

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <MobileNav />
+    <div class="justify-between hidden w-10/12 h-16 mx-auto my-10 lg:flex">
+      <nav class="self-center w-5/12">
+        <ul class="flex">
+          <li class="mr-8 text-lg text-white cursor-pointer"><router-link to="/">Portfolio</router-link></li>
+          <li class="mr-8 text-lg text-white cursor-pointer"><router-link to="/about">About Me</router-link></li>
+          <li class="mr-8 text-lg text-white cursor-pointer"><router-link to="/contact">Get In Touch</router-link></li>
+        </ul>
+      </nav>
+      <router-link to="/">
+        <img src="@/assets/Hi-di Ho.png" class="self-center w-24 h-24" />
+      </router-link>
+      <div class="flex justify-end w-5/12 h-16">
+        <a href="https://www.instagram.com/the_shred_flanders/" target="_blank" class="my-auto cursor-pointer ml-7">
+          <i class="text-2xl text-white fa-brands fa-instagram"></i>
+        </a>
+        <a href="https://www.facebook.com/profile.php?id=100083959629324" target="_blank" class="my-auto cursor-pointer ml-7">
+          <i class="text-2xl text-white fa-brands fa-facebook"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/shred-flanders-356896246/" target="_blank" class="my-auto cursor-pointer ml-7">
+          <i class="text-2xl text-white fa-brands fa-linkedin"></i>
+        </a>
+        <a href="https://twitter.com/ShredFlanders4" target="_blank" class="my-auto cursor-pointer ml-7">
+          <i class="text-2xl text-white fa-brands fa-twitter"></i>
+        </a>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import MobileNav from './components/MobileNav.vue'
+
+export default {
+  name: "App",
+  components: { MobileNav }
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <header class="w-10/12 mx-auto my-12 text-center lg:my-24">
+    <h1 class="text-4xl my-3 lg:my-5 lg:text-[3rem] font-medium text-white">Shred Flanders</h1>
+    <h2 class="text-4xl my-3 lg:my-5 lg:text-[3rem] text-green-400">Guitar Shredder</h2>
+    <h2 class="text-4xl my-3 lg:my-5 lg:text-[3rem] text-white">Web Developer</h2>
+    <img src="@/assets/Shred Flanders.png" class="h-56 mx-auto my-12 border-4 border-green-400 lg:h-96" />
+  </header>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
